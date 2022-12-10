d8097b0804e588e8aa6b0d1e4b40e60cdc79ea29
Vachna Ram <vachnaram48@gmail.com>
2022-12-11 01:13:41 +0530

    various_components_added

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import TableComponent from './components/TableComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <TableComponent/>
+      
+    </div>
+  );
+}
+
+export default App;
